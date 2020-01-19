9bb3e8670a828d3078d35a854e5e23cae661dd36
tanakaderoy <tanaka.deroy@gmail.com>
2020-01-19 04:40:55 -0600

    some examples

---
+++ src/components/examples/FirstComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+//Class component
+class FirstComponent extends Component {
+    render() {
+      return <div className="firstCompenent">FirstComponent</div>;
+    }
+  }
+
+export default FirstComponent;
---
+++ src/components/examples/ThirdComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ThirdComponent() {
+  return <div className="thirdComponent">thirdComponent</div>;
+}
+
+export default ThirdComponent;
